0494236ac88b24809f39d45246a57e5da2b9148d
daignespola <daignespola@gmail.com>
2016-11-25 10:55:50 -0300

    Add files via upload
    Creacion de personaje y almacenado en bd. Lovs dinamicas

--- bd/baseDeDatos/SQLiteConnection.java
+++ bd/baseDeDatos/SQLiteConnection.java
@@ -1,8 +1,13 @@
 package baseDeDatos;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.*;
 
+import principal.entes.personajes.Personaje;
 import principal.peticiones.CodigoPeticion;
 
 public class SQLiteConnection {
@@ -19,6 +24,7 @@ public class SQLiteConnection {
 			conn = DriverManager.getConnection("jdbc:sqlite:.\\bd\\baseDeDatos\\BDPrueba.sqlite");
 			//BORRAR
 			JOptionPane.showMessageDialog(null,"Conexión exitosa"); //se supone que debería mostrar el error
+			conn.setAutoCommit(true);
 			//BORRAR
 			return conn;
 		}catch(Exception e){
@@ -33,14 +39,15 @@ public class SQLiteConnection {
 			String query = "select * from Usuario where nombre = ? and password = ?";
 			pst= conn.prepareStatement(query); 
 			pst.setString(1, usuario); //0 es el primer ? que pongo en la query
-			pst.setString(2, password); //0 es el primer ? que pongo en la query
+			pst.setString(2, (String) password); //0 es el primer ? que pongo en la query
 			
 			ResultSet rs = pst.executeQuery();
 			if (rs.next()){
-				return CodigoPeticion.LOGEO_CORRECTO;
+				//return CodigoPeticion.LOGEO_CORRECTO;
+				return rs.getInt("ID");
 			}
 			else {
-				 return CodigoPeticion.LOGEO_INCORRECTO;
+				 return CodigoPeticion.LOGUEO_INCORRECTO;
 			}
 		}catch(SQLException sqle)
 		{
@@ -53,7 +60,7 @@ public class SQLiteConnection {
 				e.printStackTrace();
 			}
 		}
-		return CodigoPeticion.LOGEO_INCORRECTO;
+		return CodigoPeticion.LOGUEO_INCORRECTO;
 	}
 	
 	public int registro(String usuario, String password, String mail){
@@ -62,6 +69,7 @@ public class SQLiteConnection {
 		try{
 			String query = "select * from Usuario where nombre = ?";
 			pst= conn.prepareStatement(query); 
+	
 			pst.setString(1, usuario);
 			ResultSet rs = pst.executeQuery();
 			if (rs.next()){
@@ -88,5 +96,92 @@ public class SQLiteConnection {
 			}
 		}
 		return CodigoPeticion.REGISTRO_INCORRECTO;
+	}
+	
+	public int registroPersonaje(int id_usuario, String nombrePer, int raza, int casta, char sexo) {
+		PreparedStatement pst = null;
+		PreparedStatement pst2 = null;
+		try{
+			String query = "select * from Personaje where id_usuario = ? and nombre = ?";
+			pst= conn.prepareStatement(query); 
+			pst.setInt(1, id_usuario);
+			pst.setString(2, nombrePer);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()){
+				return CodigoPeticion.REGISTRO_PERSONAJE_YA_EXISTE;
+			}
+			
+			String query2 = "insert into Personaje (id_usuario,nombre, raza, casta, sexo) values (?,?,?,?,?)";
+			pst2= conn.prepareStatement(query2); 
+			pst2.setInt(1, id_usuario);
+			pst2.setString(2, nombrePer);
+			pst2.setInt(3, raza);
+			pst2.setInt(4, casta);
+			pst2.setLong(5,sexo);
+			
+			pst2.executeUpdate();		//notar que para hacer cambios en vez de consultas se usa executeUpdate()
+			return CodigoPeticion.REGISTRO_PERSONAJE_CORRECTO;
+		}catch(SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+		finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return CodigoPeticion.REGISTRO_PERSONAJE_INCORRECTO;
+	}
+	
+	public Map<Integer, String> listarRazas(){
+		PreparedStatement pst = null;
+		try{
+			String query = "SELECT * FROM Raza";
+			pst= conn.prepareStatement(query); 
+			Map <Integer,String> razasMap = new HashMap<Integer,String>();
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()){
+				razasMap.put(rs.getInt("ID_RAZA"), rs.getString("DESCRIPCION"));
+			}
+			return razasMap;
+		}catch(SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+		finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+	
+	public Map<Integer, String> listarCastas(){
+		PreparedStatement pst = null;
+		try{
+			String query = "SELECT * FROM casta";
+			pst= conn.prepareStatement(query); 
+			Map <Integer,String> castasMap = new HashMap<Integer,String>();
+			ResultSet rs = pst.executeQuery();
+			while(rs.next()){
+				castasMap.put(rs.getInt("ID_CASTA"), rs.getString("DESCRIPCION"));
+			}
+			return castasMap;
+		}catch(SQLException sqle)
+		{
+			sqle.printStackTrace();
+		}
+		finally {
+			try {
+				pst.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
 	}
 }
